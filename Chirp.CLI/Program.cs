c7481b4c5b60df099ba11e8dcbde8500996b8f96
Patrick <patrickherlin0801@gmail.com>
2023-09-01 07:33:01 +0200

    Fixed formating of time
    Formating of time and regex that splits the CSV should be more sophisticated

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System.Reflection;
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Data;
 
- class Program
+class Program
 {
+    
     static void Main(string[] args)
 {
     string cvsPath = "../Data/chirp_cli_db.csv";
@@ -14,9 +17,17 @@ using System.IO;
     }
 
     using (var reader = new StreamReader(cvsPath)){
+        var line = reader.ReadLine();
         while(!reader.EndOfStream){
-            var line = reader.ReadLine();
-            Console.WriteLine(line);
+            
+            line = reader.ReadLine();
+            var regexHandler = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+            var cvsArray = regexHandler.Split(line);
+            cvsArray[1] = cvsArray[1].Substring(1,cvsArray[1].Length - 2);
+            var timeOfChirp = cvsArray[2];
+            DateTimeOffset addedTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse((timeOfChirp)));
+            var convertedTime = addedTime.LocalDateTime;
+            Console.WriteLine(cvsArray[0] + " @ " + convertedTime + ": " + cvsArray[1]);
         }
     }
 }
