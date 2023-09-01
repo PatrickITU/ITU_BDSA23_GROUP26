0124734db7de7d3da06dd1af388922a02280685c
Patrick <patrickherlin0801@gmail.com>
2023-09-01 06:36:53 +0200

    App can not print CVS
    App can now print a primitive print of the CVA (No regex, replaced or added, just raw data from CVS)

---
+++ Chirp.CLI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System;
+using System.IO;
+
+ class Program
+{
+    static void Main(string[] args)
+{
+    string cvsPath = "../Data/chirp_cli_db.csv";
+    if (!File.Exists(cvsPath)){
+        Console.WriteLine("CVS FILE NOT FOUND");
+        Console.WriteLine(Directory.GetCurrentDirectory());
+        return;
+    }
+
+    using (var reader = new StreamReader(cvsPath)){
+        while(!reader.EndOfStream){
+            var line = reader.ReadLine();
+            Console.WriteLine(line);
+        }
+    }
+}
+}
